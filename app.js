54e766b675e971dbe2815cc081f723773abf6e47
MLDODSON <mldodson@fullsail.edu>
2021-08-03 11:47:24 -0500

    JS Basics
    JS Basics. Declaring and invoking functions.

---
+++ app.js
@@ -0,0 +1,22 @@
+/* FUNCTIONS */
+
+// Declaring and invoking 
+function hi() {
+    // logic
+    console.log('Hola Glenny!');
+    console.log('Hola Paola!');
+}
+
+hi();
+
+function bye() {
+    console.log('Adios Luisa!');
+    console.log("Adios Margaret!");
+}
+
+bye();
+
+
+
+
+
